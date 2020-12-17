27ce8e7f4bc7ff85dd33bef184cdaf0aa2b0e7d1
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-12-17 20:49:56 +0200

    добавляю фундамент проекта

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import './App.css';
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
 
 function App() {
-  return <div className="App"></div>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
